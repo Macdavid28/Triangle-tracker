2f0c45bf1197a54d6aff7cf538cad454dfc245b7
David <david.tomi2020@gmail.com>
2024-03-07 21:58:20 +0100

    create input forms for triangle length

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function triangleTracker() {
   firstInput = document.getElementById("input1").value;
   secondInput = document.getElementById("input2").value;
   thirdInput = document.getElementById("input3").value;
-//   
+  //
   if (
     (firstInput === secondInput && secondInput === thirdInput) ||
     (thirdInput === firstInput && firstInput === secondInput) ||
     (secondInput === thirdInput && thirdInput === firstInput)
   ) {
-
+    document.getElementById("result").innerHTML = "Equilateral";
   }
 }
