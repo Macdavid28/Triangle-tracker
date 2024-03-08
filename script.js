d945e409a0c3df4b9f4a0b3a881f47897ca9eaf5
David <david.tomi2020@gmail.com>
2024-03-08 21:28:18 +0100

    add condition for not a triangle

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 function triangleTracker() {
-  firstInput = document.getElementById("input1").value;
-  secondInput = document.getElementById("input2").value;
-  thirdInput = document.getElementById("input3").value;
-  //
+  const firstInput = parseInt(document.getElementById("input1").value);
+  const secondInput = parseInt(document.getElementById("input2").value);
+  const thirdInput = parseInt(document.getElementById("input3").value);
+  // Not a  triangle
   if (
+    (firstInput === 0 && secondInput === 0 && thirdInput === 0) ||
+    firstInput + secondInput <= thirdInput ||
+    secondInput + thirdInput <= firstInput ||
+    thirdInput + firstInput <= secondInput
+  ) {
+    document.getElementById("result").innerHTML = "Not a triangle";
+  }
+  // Equilateral Triangle
+  else if (
     (firstInput === secondInput && secondInput === thirdInput) ||
     (thirdInput === firstInput && firstInput === secondInput) ||
     (secondInput === thirdInput && thirdInput === firstInput)
   ) {
-    document.getElemeById("result").innerHTML = "Equilateral ";
+    document.getElementById("result").innerHTML = "Equilateral Triangle";
+  }
+  // Isosceles Triangle
+  else if (
+    firstInput === secondInput ||
+    secondInput === thirdInput ||
+    firstInput === thirdInput
+  ) {
+    document.getElementById("result").innerHTML = "Isosceles Triangle";
+  }
+  // Scalene Triangle
+  else if (
+    (firstInput != secondInput &&
+      secondInput != thirdInput &&
+      firstInput != thirdInput &&
+      firstInput + secondInput > thirdInput) ||
+    secondInput + thirdInput > firstInput ||
+    thirdInput + secondInput > firstInput
+  ) {
+    console.log("Scalene Triangle");
+    document.getElementById("result").innerHTML = "Scalene Triangle";
   }
 }
